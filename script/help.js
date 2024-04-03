2d905a55eeabd76e4ccb2f5698a07a93ad7383c3
xAnselmo <164136289+xAnselmo@users.noreply.github.com>
2024-04-03 20:58:17 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -25,7 +25,7 @@ module.exports.run = async function({
 			let page = 1;
 			let start = (page - 1) * pages;
 			let end = start + pages;
-			let helpMessage = `🔴🟢🟡\n\n====『 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧: 』====\n
+			let helpMessage = `𝗕𝗢𝗕𝗢 𝗞𝗔\n\n====『 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧: 』====\n
 ▱▱▱▱▱▱▱▱▱▱▱▱▱\n\n`;
 			for (let i = start; i < Math.min(end, commands.length); i++) {
 				helpMessage += `╭─╮\n |\t『 ${i + 1}.』  ${prefix}${commands[i]}\n╰─────────────ꔪ\n`;
